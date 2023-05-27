249f2c0c2408f60a7ad6b3ac8e3c86be7699ce52
Alpertunga Ertin <ertin@student.tugraz.at>
2023-05-27 07:49:30 +0200

    Parse date column

--- app.R
+++ app.R
@@ -3,3 +3,13 @@
 # load the dataset -helps both the sub group
 # ENable total profit, sales and region dropdown
 # TImes series and Pie chart
+
+rm(list=ls()) # Clears memory
+graphics.off() # Clears graphs
+if (!require("pacman")) install.packages("pacman") #Installs package for package installation
+pacman::p_load("shiny", "tidyverse","dygraphs","xts","tidyr","lubridate")
+
+sales_data <- read.csv("Supermart Grocery Sales - Retail Analytics Dataset.csv", header= TRUE, sep = ",")
+
+sales_data$date<-as.Date(lubridate::parse_date_time(sales_data$Order.Date, orders = c('mdy')))
+
