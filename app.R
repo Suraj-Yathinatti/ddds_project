27f876148b7cd9b24b67f8de0822f9d222c0ff1d
Alpertunga Ertin <ertin@student.tugraz.at>
2023-05-27 08:37:35 +0200

    Filter by region

--- app.R
+++ app.R
@@ -10,6 +10,60 @@ if (!require("pacman")) install.packages("pacman") #Installs package for package
 pacman::p_load("shiny", "tidyverse","dygraphs","xts","tidyr","lubridate")
 
 sales_data <- read.csv("Supermart Grocery Sales - Retail Analytics Dataset.csv", header= TRUE, sep = ",")
+sales_data$regions<- as.factor(sales_data$Region)
 
 sales_data$date<-as.Date(lubridate::parse_date_time(sales_data$Order.Date, orders = c('mdy')))
 
+ui <- fluidPage(
+  titlePanel("Sales Data"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("selected_region", label = h3("Select the region"), 
+              choices = c("all",levels(sales_data$regions))),
+      hr(),
+      fluidRow(column(3, verbatimTextOutput("value"))),
+      fluidRow(column(3, verbatimTextOutput("totalSales"))),
+      fluidRow(column(3, verbatimTextOutput("totalProfit")))
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  output$value <- renderPrint({ input$selected_region })
+  output$totalSales<-renderPrint({  if(input$selected_region=="all"){
+    sum(sales_data$Sales)
+  }else{
+    sales_data %>% filter(Region==input$selected_region ) %>% select("Sales") %>%  sum()
+  }
+    })
+
+  
+  output$totalProfit<-renderPrint({ if(input$selected_region=="all"){
+    sum(sales_data$Profit)
+  }else{
+    sales_data %>% filter(Region==input$selected_region ) %>% select("Profit") %>% sum()
+  } })
+  output$distPlot <- renderPlot({
+  
+    if (input$selected_region=="all"){
+    sales_data %>%
+      ggplot(aes(date,Sales)) +
+      geom_line() 
+
+    }
+    else{
+    sales_data %>%
+      filter(Region==input$selected_region ) %>%
+      ggplot(aes(date,Sales)) +
+      geom_line() 
+   
+    }
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
